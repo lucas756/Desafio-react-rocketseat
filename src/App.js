7085d41302c2d4aa085062903a96dcbe9817dd06
Lucas <salleslucas00@gmail.com>
2020-12-26 22:49:21 -0300

    desafio concluido

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, {useEffect, useState} from "react";
+import api from "./services/api";
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() =>{
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+
+    });
+  }, []);
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: 'LucasJS',
+      url: 'https://github.com/lucas756',
+      techs: ['ReactJS', 'ReactNative'],
+    }) //pegar o resultado da API
+    
+    setRepositories([...repositories, response.data])
+  
+  }
+
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(
+      repository => repository.id != id
+    ))
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repositoriy => (
+             <li key={repositoriy.id}>
+             {repositoriy.title}
+   
+             <button onClick={() => handleRemoveRepository(repositoriy.id)}>
+               Remover
+             </button>
+           </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
